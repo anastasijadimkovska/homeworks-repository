e8d529df0f941009462abd0e98981ad3218d64ab
anastasijadimkovska <anastasijad53@gmail.com>
2022-02-08 20:58:42 +0100

    starwars api workshop(NOT FINISHED)

---
+++ vezba12/script.js
@@ -0,0 +1,43 @@
+
+
+const printName = (element, name) => element.innerText = name;
+
+const createTd = value => {
+    const td = document.createElement('td')
+    td.classList.add("table-dark")
+    td.innerText = value;
+    return td;
+};
+const createTr = (key, value) => {
+    const tr = document.createElement('tr');
+    tr.classList.add("table-dark")
+    tr.appendChild(createTd(key));
+    tr.appendChild(createTd(value));
+    return tr;
+};
+
+const makeTable = (element, data) => {
+    const keys = Object.keys(data);
+    keys.forEach(key => {
+        if(typeof data[key] === 'string') {
+            element.append(createTr(key, data[key]))
+        }
+    })};
+
+    $('.rocketship').click(() =>{
+        $(function() {
+            $.ajax({
+                url: 'https://swapi.dev/api/starships/9/',
+                success: res => {
+                    const table = $('#result');
+                    table.addClass("table table-dark table-hover")
+                    console.log({x: res.name});
+                    printName(res.name);
+                    makeTable(table, res)
+                },
+                error: err => {
+                    console.log
+                }
+            })
+        });
+     })
