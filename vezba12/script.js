636ab3654bf70a1f8c55fde41576c1f3851e003f
anastasijadimkovska <anastasijad53@gmail.com>
2022-03-04 17:33:49 +0100

    Workshop Updated
    Updated Workshop Star Wars
    Anastasija G7

--- vezba12/script.js
+++ vezba12/script.js
@@ -1,12 +1,12 @@
 const peopleSection = document.querySelector('.people');
 const shipsSection = document.querySelector('.ships');
 let nextBtn = document.querySelector('#next');
-
+ 
 let prevBtn = document.querySelector('#previous');
 const shipsTable = document.querySelector('#shipsData');
 const homeSection = document.querySelector('.wrapper')
 const peopleTable = document.querySelector('#peopleData');
-
+ 
 class Person {
     constructor(obj) {
         const { name, height, mass, gender, birth_year, homeworld, films } = obj;
@@ -19,7 +19,7 @@ class Person {
         this.appearances = Array.isArray(films) && films.length || null;
     }
 }
-
+ 
 class ShipDetails {
     constructor(obj) {
         const { cost_in_credits, cargo_capacity, passengers, starship_class } = obj;
@@ -29,7 +29,7 @@ class ShipDetails {
         this.class = starship_class;
     }
 }
-
+ 
 class Ship extends ShipDetails {
     constructor({ name, model, manufacturer, cost_in_credits, cargo_capacity, passengers, starship_class }) {
         const obj = { cost_in_credits, cargo_capacity, passengers, starship_class };
@@ -39,8 +39,8 @@ class Ship extends ShipDetails {
         this.manufacturer = manufacturer;
     }
 }
-
-
+ 
+ 
 const memory = {
     url: 'https://swapi.dev/api/',
     currentContext: '',
@@ -50,107 +50,104 @@ const memory = {
     previous: null,
     next: null
 };
-
-
+ 
+ 
 async function fetchData(url, context) {
     let call = await fetch(url);
     let dataJson = await call.json();
+    refreshMemory(dataJson, context);
     memory.currentContext = context;
     return dataJson;
 }
-
-
+ 
+ 
 function refreshMemory(r, context) {
     memory.previous = r.previous;
     memory.next = r.next;
     memory.data = r.results.map(data => context === memory.people ? new Person(data) : new Ship(data));
 }
-async function getPeople(url) {
-    const data = await fetchData(url, memory.people);
-    refreshMemory(data, memory.people);
+ 
+ 
+function generateHumanTable(e) {
+    let person = new Person(e);
+    let row = document.createElement('tr');
+    row.innerHTML +=
+        `<td>${person.name}</td>
+        <td>${person.height}</td>
+        <td>${person.mass}</td>
+        <td>${person.gender}</td>
+        <td>${person.birthYear}</td>
+        <td>${person.homeworld}</td>
+        <td>${person.appearances}</td>`;
+ 
+    peopleTable.append(row)
 }
-async function getShips(url) {
-    const data = await fetchData(url, memory.starships);
-    refreshMemory(data, memory.ships);
+ 
+function generateShipTable(e) {
+    let ship = new Ship(e);
+    let row = document.createElement('tr');
+    row.innerHTML +=
+        `<td>${ship.name}</td>
+        <td>${ship.model}</td>
+        <td>${ship.manufacturer}</td>
+        <td>${ship.cost}</td>
+        <td>${ship.cargoCapacity}</td>
+        <td>${ship.peopleCapacity}</td>
+        <td>${ship.class}</td>`;
+ 
+    shipsTable.append(row)
 }
-
-document.querySelector('.human').addEventListener('click', async function () {
-   
+ 
+function refreshTable() {
     peopleTable.innerHTML = '';
+    shipsTable.innerHTML = '';
+}
+ 
+document.querySelector('.human').addEventListener('click', async function () {
+    refreshTable();
     peopleSection.toggleAttribute('hidden');
     homeSection.toggleAttribute('hidden');
     nextBtn.toggleAttribute('hidden')
-    const humanData = await fetchData(`${memory.url}${memory.people}`)
-    console.log(humanData);
+    const humanData = await fetchData(`${memory.url}${memory.people}`, memory.people)
     humanData.results.forEach(e => {
-        let person = new Person(e);
-       
-        let row = document.createElement('tr');
-        row.innerHTML +=
-            `<td>${person.name}</td>
-            <td>${person.height}</td>
-            <td>${person.mass}</td>
-            <td>${person.gender}</td>
-            <td>${person.birthYear}</td>
-            <td>${person.homeworld}</td>
-            <td>${person.appearances}</td>`;
-       
-        peopleTable.append(row)
+        generateHumanTable(e);
     });
-
-})
-
+});
+ 
 document.querySelector('#rocketshipWrapper').addEventListener('click', async function () {
-  
-    shipsTable.innerHTML = '';
-    const shipsData = await fetchData(`${memory.url}${memory.starships}`)
+    refreshTable();
+    const shipsData = await fetchData(`${memory.url}${memory.starships}`, memory.starships)
     shipsSection.toggleAttribute('hidden');
     homeSection.toggleAttribute('hidden');
     nextBtn.toggleAttribute('hidden');
-    console.log(shipsData);
     shipsData.results.forEach(e => {
-        let ship = new Ship(e);
-       
-        let row = document.createElement('tr');
-        row.innerHTML +=
-            `<td>${ship.name}</td>
-            <td>${ship.model}</td>
-            <td>${ship.manufacturer}</td>
-            <td>${ship.cost}</td>
-            <td>${ship.cargoCapacity}</td>
-            <td>${ship.peopleCapacity}</td>
-            <td>${ship.class}</td>`;
-       
-        shipsTable.append(row)
+        generateShipTable(e);
     });
 });
-
-
-
+ 
+ 
+ 
 nextBtn.addEventListener('click', async () => {
     prevBtn.removeAttribute('hidden');
-    if (memory.next) {
-        
+    const data = await fetchData(memory.next, memory.currentContext);
+    refreshTable();
+    data.results.forEach(e => {
         if (memory.currentContext === memory.people) {
-            getPeople(memory.next);
+            generateHumanTable(e);
         } else if (memory.currentContext === memory.starships) {
-            getShips(memory.next);
+            generateShipTable(e)
         }
-    }
-
-})
-
+    });
+});
+ 
 prevBtn.addEventListener('click', async () => {
-
-    if (memory.previous) {
-    
+    const data = await fetchData(memory.previous, memory.currentContext);
+    refreshTable();
+    data.results.forEach(e => {
         if (memory.currentContext === memory.people) {
-            getPeople(memory.previous);
+            generateHumanTable(e);
         } else if (memory.currentContext === memory.starships) {
-            getShips(memory.previous);
+            generateShipTable(e)
         }
-    }
-
-})
-
-
+    });
+});
