8fd0b333ae00e7a25eb4d2b8946173ca18eba0e2
anastasijadimkovska <anastasijad53@gmail.com>
2022-03-21 01:04:50 +0100

    NodeJS Homework#2 Uploaded
    without bonus anastasija g10

--- Homework#2_NodeJS/index.js
+++ Homework#2_NodeJS/index.js
@@ -3,18 +3,8 @@ const path = require("path");
 const newPath = path.join(__dirname, "index.js");
 const filePath = path.join(__dirname, "homework.txt");
 
-fs.writeFile(filePath, 'Homework 02 in Basic Node', function (err) {
-    if (err){throw new Error('File not saved')};
-    console.log('File saved');
-  });
-  fs.appendFile(filePath, ' FINISHED!', function (err) {
-    if (err) {throw new Error('File not updated')};
-    console.log('File updated');
-  });
+fs.writeFileSync(filePath, 'Homework 02 in Basic Node',);
+fs.appendFileSync(filePath, ' FINISHED!'); 
   
-  
-  fs.readFile(filePath, { encoding: "utf-8" }, (err,data) => {
-      if (err) throw new Error("Something went wrong while reading file");
-        console.log(`This is the file content:  ${data}`);
-    });
-
+ const data =  fs.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(`This is the file content:  ${data}`)
