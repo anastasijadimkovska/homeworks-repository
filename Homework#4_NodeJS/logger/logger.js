32d6de367850cb16729552850e1c0b6ba26d0138
anastasijadimkovska <anastasijad53@gmail.com>
2022-04-05 18:05:50 +0200

    homework 4 events

--- Homework#4_NodeJS/logger/logger.js
+++ Homework#4_NodeJS/logger/logger.js
@@ -1,24 +1,24 @@
-const fs = require ('fs');
-const path = require ('path');
+const fs = require('fs');
+const path = require('path');
 const EventEmitter = require("events");
-class MyEmitter extends EventEmitter {};
+class MyEmitter extends EventEmitter { };
 const emitter = new MyEmitter();
 
 const statsPath = path.join(__dirname, 'stats.json');
-const statsData = JSON.parse(fs.readFileSync(statsPath, {encoding:'utf-8'}));
+const statsData = JSON.parse(fs.readFileSync(statsPath, { encoding: 'utf-8' }));
 
 emitter.on('request', () => {
     statsData.numberOfRequests += 1;
     fs.writeFileSync(statsPath, JSON.stringify(statsData));
 }).on('student-added', () => {
-statsData.numberOfStudents += 1;
-fs.writeFileSync(statsPath, JSON.stringify(statsData));
+    statsData.numberOfStudents += 1;
+    fs.writeFileSync(statsPath, JSON.stringify(statsData));
 }).on('student-name-logged', () => {
     statsData.studentNames.push(statsData);
     fs.writeFileSync(statsPath, JSON.stringify(statsData));
 });
 emitter.once('server-started', () => {
     console.log(`Server is up and running`)
-  })
+})
 
 module.exports = emitter;
--- Homework#4_NodeJS/server.js
+++ Homework#4_NodeJS/server.js
@@ -5,60 +5,60 @@ const studentPath = path.join(__dirname, "logs.txt");
 const emitter = require('./logger/logger')
 
 
-const server = http.createServer((request, response) => { 
-    const url = request.url;
-    const method = request.method;  
-    emitter.emit('request');
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, DELETE, PUT, OPTIONS"
-    );
-    emitter.emit("request");
-    if (url === "/") {
-        response.setHeader("Content-Type", "text/html");
-        response.write(`<h1>Hello Welcome to My Server</h1>`);
-        return response.end();
-      }
-      if (url === '/students' && method === 'GET') {
-        response.setHeader('Content-Type", "text/html');
-        response.write('<h1>hello students</h1>')
-        const studentData = fs.readFileSync(studentPath,  { encoding: "utf-8" });
-        return response.end(studentData);
-      }
-      if (url ==='/students' && method === 'POST') {
-         const chunksReceived = [];
-        request.on("data", chunk => {
-          console.log(chunk);
-          chunksReceived.push(chunk);
-        });
-        request.on("end", () => {
-          const parsedData = Buffer.concat(chunksReceived).toString();    
-          const studentsData = fs.readFileSync(studentPath, { encoding: "utf-8" });
-          const parsedstudentsData = JSON.parse(studentsData);
-    
-          const parsedJsonstudent = JSON.parse(parsedData);
-          console.log(parsedJsonstudent);
-          parsedstudentsData.students.push(parsedJsonstudent);
-    
-          const stingifiedstudentsToBeSaved = JSON.stringify(parsedstudentsData);
-            emitter.emit("student-added");
-            emitter.emit('student-name-logged');
-            fs.writeFileSync(studentsPath, stingifiedstudentsToBeSaved, {
-            encoding: "utf-8",
-          });
-        });
-        
-        return response.end();
-      }
-      else {  
-        response.statusCode = 302;
-        response.setHeader("Location", "/");
-        return response.end();
-      }
+const server = http.createServer((request, response) => {
+  const url = request.url;
+  const method = request.method;
+  emitter.emit('request');
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, DELETE, PUT, OPTIONS"
+  );
+  emitter.emit("request");
+  if (url === "/") {
+    response.setHeader("Content-Type", "text/html");
+    response.write(`<h1>Hello Welcome to My Server</h1>`);
+    return response.end();
+  }
+  if (url === '/students' && method === 'GET') {
+    response.setHeader('Content-Type", "text/html');
+    response.write('<h1>hello students</h1>')
+    const studentData = fs.readFileSync(studentPath, { encoding: "utf-8" });
+    return response.end(studentData);
+  }
+  if (url === '/students' && method === 'POST') {
+    const chunksReceived = [];
+    console.log(1)
+    request.on("data", chunk => {
+      console.log(chunk);
+      chunksReceived.push(chunk);
+    });
+    request.on("end", () => {
+      const parsedData = Buffer.concat(chunksReceived).toString();
+      const studentsData = fs.readFileSync(studentPath, { encoding: "utf-8" });
+      const parsedstudentsData = JSON.parse(studentsData);
+
+      const parsedJsonstudent = JSON.parse(parsedData);
+      console.log(parsedJsonstudent);
+      parsedstudentsData.students.push(parsedJsonstudent);
+
+      const stingifiedstudentsToBeSaved = JSON.stringify(parsedstudentsData);
+      emitter.emit("student-added");
+      emitter.emit('student-name-logged');
+      fs.writeFileSync(studentsPath, stingifiedstudentsToBeSaved, {
+        encoding: "utf-8",
+      });
+    });
+
+    return response.end();
+  }
+  else {
+    response.statusCode = 302;
+    response.setHeader("Location", "/");
+    return response.end();
+  }
 })
 
 server.listen(3000, "localhost", () => {
   emitter.emit('server-started')
-  });
-  
+});
